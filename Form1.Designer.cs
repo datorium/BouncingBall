e22588bbe8b11ba46cfb26c0dbb8aa23db9e87d2
Datorium <hello@datorium.lv>
2020-04-28 17:08:41 +0300

    version 17:08

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,34 +29,48 @@
         private void InitializeComponent()
         {
             this.Ball = new System.Windows.Forms.PictureBox();
+            this.SpeedLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
             this.SuspendLayout();
             // 
             // Ball
             // 
             this.Ball.BackColor = System.Drawing.Color.Coral;
-            this.Ball.Location = new System.Drawing.Point(242, 123);
+            this.Ball.Location = new System.Drawing.Point(349, 135);
             this.Ball.Name = "Ball";
             this.Ball.Size = new System.Drawing.Size(100, 100);
             this.Ball.TabIndex = 0;
             this.Ball.TabStop = false;
+            // 
+            // SpeedLabel
+            // 
+            this.SpeedLabel.AutoSize = true;
+            this.SpeedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SpeedLabel.Location = new System.Drawing.Point(12, 9);
+            this.SpeedLabel.Name = "SpeedLabel";
+            this.SpeedLabel.Size = new System.Drawing.Size(122, 44);
+            this.SpeedLabel.TabIndex = 1;
+            this.SpeedLabel.Text = "label1";
             // 
             // Field
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SpeedLabel);
             this.Controls.Add(this.Ball);
             this.Name = "Field";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Label SpeedLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,7 @@ namespace BouncingBall
     {
         private int verVelocity = 0;
         private int horVelocity = 0;
-        private int speed = 3;
+        private int speed = 2;
 
         private Timer mainTimer = null;
 
@@ -30,6 +30,8 @@ namespace BouncingBall
             verVelocity = speed;
             horVelocity = speed;
             this.BackColor = Color.LightBlue;
+            this.KeyDown += new KeyEventHandler(Field_KeyDown);
+            UpdateSpeedLabel();
         }
         
         private void InitializeMainTimer()
@@ -43,6 +45,7 @@ namespace BouncingBall
         private void MainTimer_Tick(object sender, EventArgs e)
         {
             MoveTheBall();
+            BallBorderCollision();
         }
 
         private void MoveTheBall()
@@ -51,5 +54,48 @@ namespace BouncingBall
             Ball.Left += horVelocity;
         }
 
+        private void BallBorderCollision()
+        {
+            if(Ball.Top + Ball.Height > ClientRectangle.Height)
+            {
+                verVelocity = -speed;
+            }
+            else if (Ball.Top < 0)
+            {
+                verVelocity = speed;
+            }
+            if (Ball.Left + Ball.Width > ClientRectangle.Width)
+            {
+                horVelocity = -speed;
+            }
+            else if (Ball.Left < 0)
+            {
+                horVelocity = speed;
+            }
+        }
+
+        private void Field_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.X)
+            {
+                speed += 1; //speed++
+
+                UpdateSpeedLabel();
+            }
+            else if(e.KeyCode == Keys.Z)
+            {
+                if(speed > 1)
+                {
+                    speed -= 1; //speed--
+
+                    UpdateSpeedLabel();
+                }                
+            }
+        }
+
+        private void UpdateSpeedLabel()
+        {
+            SpeedLabel.Text = "Ball speed: " + speed;
+        }
     }
 }
