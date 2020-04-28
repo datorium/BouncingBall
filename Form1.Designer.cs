591cd9c8275a07ab7c7e32b6c44382f34583b9f7
Datorium <hello@datorium.lv>
2020-04-28 15:58:15 +0300

    version zero

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,35 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Ball = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Ball
-            // 
-            this.Ball.BackColor = System.Drawing.Color.OrangeRed;
-            this.Ball.Location = new System.Drawing.Point(326, 185);
-            this.Ball.Name = "Ball";
-            this.Ball.Size = new System.Drawing.Size(100, 98);
-            this.Ball.TabIndex = 0;
-            this.Ball.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Ball);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox Ball;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,35 +12,9 @@ namespace BouncingBall
 {
     public partial class Form1 : Form
     {
-        int horVelocity = 0;
-        int verVelocity = 0;
-        int ballStep = 3;
-
-        Timer mainTimer = null;
-
         public Form1()
         {
             InitializeComponent();
-            InitializeMainTimer();
-        }
-
-        private void InitializeMainTimer()
-        {
-            mainTimer = new Timer();
-            mainTimer.Tick += new EventHandler(MainTimer_Tick);
-            mainTimer.Interval = 20;
-            mainTimer.Start();
-        }
-
-        private void MainTimer_Tick(object sender, EventArgs e)
-        {
-            MoveBall();
-        }
-
-        private void MoveBall()
-        {
-            Ball.Top += verVelocity;
-            Ball.Left += horVelocity;
-        }
+        }        
     }
 }
